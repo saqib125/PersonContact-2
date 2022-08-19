e2d6aefd6949fa8c96dbff88e8615ded586d6829
Saqib Siddique <saqib125.siddique@gmail.com>
2022-08-19 07:41:03 -0700

    Initial Commit

---
+++ PersonContact/PersonContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonContactApp.swift
+//  PersonContact
+//
+//  Created by Saqib Siddique on 2022-08-19.
+//
+
+import SwiftUI
+
+@main
+struct PersonContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
